f5f3a2fa1f72cc1dcc5b04d00d618a6060ceab5d
N14438 <nicoleta.leonte@eon-romania.ro>
2019-04-17 11:31:43 +0300

    Exceptions and abstract classes plus xml

---
+++ src/main/java/practices/SwapValues.java
@@ -0,0 +1,21 @@
+package practices;
+
+public class SwapValues {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 2;
+
+        System.out.println("Inainte de swap: ");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("Dupa swap: ");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+
+}
